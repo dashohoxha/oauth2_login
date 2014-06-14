76b82e156237bda65aa4bdfae63e545438ea874f
Dashamir Hoxha <dashohoxha@gmail.com>
2014-06-14 04:49:15 +0000

    Fixed hook_oauth2_login_enabled().

--- oauth2_login.api.php
+++ oauth2_login.api.php
@@ -37,9 +37,10 @@ function oauth2_login($destination =NULL, $destination_error =NULL) {
  * admin/config/people/oauth2_login is saved.  It gives a chance to
  * the other modules to change their configuration depending on
  * whether oauth2 login is enabled or disabled. The example below
- * hides or shows the menu link for local login.
+ * enables/disables the menu link for user/login and user/oauth2_login.
  */
 function hook_oauth2_login_enabled($enabled) {
+  // Enable or disable the link 'user/login'.
   db_update('menu_links')
     ->fields(array(
         'hidden' => ($enabled ? 1 : 0),
@@ -50,4 +51,19 @@ function hook_oauth2_login_enabled($enabled) {
     ->condition('plid', 0)
     ->condition('module', 'menu')
     ->execute();
+
+  // Disable or enable the link 'user/oauth2_login'.
+  db_update('menu_links')
+    ->fields(array(
+        'hidden' => ($enabled ? 0 : 1),
+      ))
+    ->condition('menu_name', 'user-menu')
+    ->condition('link_path', 'user/oauth2_login')
+    ->condition('router_path', 'user/oauth2_login')
+    ->condition('plid', 0)
+    ->condition('module', 'system')
+    ->execute();
+
+  // Clear the cache and rebuild the menu.
+  menu_cache_clear('user-menu');
 }
